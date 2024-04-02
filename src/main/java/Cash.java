713fc4e71ee4f531379c75b5b3379b890df1339b
chris.tmars <chris.tmars@kakaocorp.com>
2024-04-02 12:06:04 +0900

    feat: Cash 클래스 구현

---
+++ src/main/java/Cash.java
@@ -0,0 +1,26 @@
+public class Cash {
+
+	private final int value;
+
+	public Cash(int value) {
+		validateNegative(value);
+		validateLessThanTicketPrice(value);
+		this.value = value;
+	}
+
+	private void validateNegative(int value) {
+		if (value < 0) {
+			throw new IllegalArgumentException("Cash의 값은 음수가 아니여야 합니다.");
+		}
+	}
+
+	private void validateLessThanTicketPrice(int value) {
+		if (value < LottoTicket.PRICE) {
+			throw new IllegalArgumentException("잔여 Cash가 부족합니다.");
+		}
+	}
+
+	public int getTicketCount() {
+		return value / LottoTicket.PRICE;
+	}
+}
